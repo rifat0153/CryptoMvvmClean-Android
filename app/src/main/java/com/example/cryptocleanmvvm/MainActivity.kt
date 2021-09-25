d9a0418fd23e7518434030471659a9c434d8ad84
Mahbubur Rahman <rifat0153@gmail.com>
2021-09-25 19:34:37 +0600

    Changing build.gradle for Navigation

--- app/src/main/java/com/example/cryptocleanmvvm/MainActivity.kt
+++ app/src/main/java/com/example/cryptocleanmvvm/MainActivity.kt
@@ -13,14 +13,16 @@ import com.example.cryptocleanmvvm.presentation.ui.theme.CryptoCleanMvvmTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            CryptoCleanMvvmTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
-        }
+//        setContent {
+//            CryptoCleanMvvmTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(color = MaterialTheme.colors.background) {
+//                    Greeting("Android")
+//                }
+//            }
+//        }
+
+        setContentView(R.layout.activity_main)
     }
 }
 
---
+++ app/src/main/java/com/example/cryptocleanmvvm/presentation/coin_detail/CoinDetailFragment.kt
@@ -0,0 +1,28 @@
+package com.example.cryptocleanmvvm.presentation.coin_detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+
+class CoinDetailFragment : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return ComposeView(requireContext()).apply { 
+            setContent { 
+                Column() {
+                    Text(text = "Coin Detail View")
+                }
+            }
+        }
+    }
+    
+}
